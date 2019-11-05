d734a59b47621a8af76428d9373a5e2b087d0efe
Ha Pham <hapham@Has-MacBook-Pro.local>
2019-11-05 18:56:10 -0500

    manipulating the fixtures data, created init function and implemented beforeEach but the test didn't pass, moving on

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -1,64 +1,67 @@
-//The file will export an object with the three functions that we use in the useApplicationData Hook.
-//It is important that the name of the module matches the name of the library that we are mocking
-//Since we are mocking the get function, we need to use jest.fn() to create the mock.
-//We want to control the data that is returned from the mock, so we need to provide an implementation.
+let fixtures = {}
 
-const fixtures = {
-  days: [
+const init = function () {//
+  fixtures = 
     {
-      id: 1,
-      name: "Monday",
-      appointments: [1, 2],
-      interviewers: [1, 2],
-      spots: 1
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      appointments: [3, 4],
-      interviewers: [3, 4],
-      spots: 1
-    }
-  ],
-  appointments: {
-    "1": { id: 1, time: "12pm", interview: null },
-    "2": {
-      id: 2,
-      time: "1pm",
-      interview: { student: "Archie Cohen", interviewer: 2 }
-    },
-    "3": {
-      id: 3,
-      time: "2pm",
-      interview: { student: "Leopold Silvers", interviewer: 4 }
-    },
-    "4": { id: 4, time: "3pm", interview: null }
-  },
-  interviewers: {
-    "1": {
-      id: 1,
-      name: "Sylvia Palmer",
-      avatar: "https://i.imgur.com/LpaY82x.png"
-    },
-    "2": {
-      id: 2,
-      name: "Tori Malcolm",
-      avatar: "https://i.imgur.com/Nmx0Qxo.png"
-    },
-    "3": {
-      id: 3,
-      name: "Mildred Nazir",
-      avatar: "https://i.imgur.com/T2WwVfS.png"
-    },
-    "4": {
-      id: 4,
-      name: "Cohana Roy",
-      avatar: "https://i.imgur.com/FK8V841.jpg"
-    }
-  }
-};
+      days: [
+        {
+          id: 1,
+          name: "Monday",
+          appointments: [1, 2],
+          interviewers: [1, 2],
+          spots: 1
+        },
+        {
+          id: 2,
+          name: "Tuesday",
+          appointments: [3, 4],
+          interviewers: [3, 4],
+          spots: 1
+        }
+      ],
+      appointments: {
+        "1": { id: 1, time: "12pm", interview: null },
+        "2": {
+          id: 2,
+          time: "1pm",
+          interview: { student: "Archie Cohen", interviewer: 2 }
+        },
+        "3": {
+          id: 3,
+          time: "2pm",
+          interview: { student: "Leopold Silvers", interviewer: 4 }
+        },
+        "4": { id: 4, time: "3pm", interview: null }
+      },
+      interviewers: {
+        "1": {
+          id: 1,
+          name: "Sylvia Palmer",
+          avatar: "https://i.imgur.com/LpaY82x.png"
+        },
+        "2": {
+          id: 2,
+          name: "Tori Malcolm",
+          avatar: "https://i.imgur.com/Nmx0Qxo.png"
+        },
+        "3": {
+          id: 3,
+          name: "Mildred Nazir",
+          avatar: "https://i.imgur.com/T2WwVfS.png"
+        },
+        "4": {
+          id: 4,
+          name: "Cohana Roy",
+          avatar: "https://i.imgur.com/FK8V841.jpg"
+        }
+      }
+    };
+}
 
 export default {
+  initFixtures() {//
+    init()
+  },
   defaults: { baseURL: "" },
   get: jest.fn(url => {
     if (url === "/api/days") {
@@ -90,12 +93,12 @@ export default {
       status: 204,
       statusText: "No content"
     });
+  }),
+  delete: jest.fn(url => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No content"
+    });
   })
-  // delete: jest.fn(url => {
-  //   return Promise.resolve({
-  //     status: 204,
-  //     statusText: "No content"
-  //   });
-  // })
   
 };
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -10,16 +10,25 @@ import {
   getByAltText,
   getAllByTestId,
   queryByText,
+  queryByAltText,
   getByPlaceholderText
 } from "@testing-library/react";
 
 import Application from "components/Application";
+//import axios from "../../__mocks__/axios";
+import axios from "axios";
 
 afterEach(cleanup);
+
 //We want to wait for the fake API request to complete before we confirm that the data has loaded
 //We can make our test asynchronous by returning a Promise.
 
 describe("Application", () => {
+  beforeEach(() => {//reset state before each it
+    cleanup();
+    axios.initFixtures()
+  });
+  
   it("defaults to Monday and changes the schedule when a new day is selected", async () => {
     const { getByText } = render(<Application />);
 
@@ -51,14 +60,44 @@ describe("Application", () => {
     fireEvent.click(getByText(appointment, "Save"))
     expect(getByText(appointment, "Saving")).toBeInTheDocument();
     await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
-    expect(getByText(appointment, "Lydia Miller-Jones")).toBeInTheDocument();
+    //expect(getByText(appointment, "Lydia Miller-Jones")).toBeInTheDocument();
     //console.log(prettyDOM(container))
     
     const day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
     );
     
-    console.log(prettyDOM(day));
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+  });
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    // 1. Render the Application.
+    const { container } = render(<Application />);
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    // 3. Click the "Delete" button on the booked appointment.
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+    fireEvent.click(queryByAltText(appointment, "Delete"));
+    //console.log(prettyDOM(container))
+    // 4. Check that the confirmation message is shown.
+    expect(getByText(appointment, "Delete the Appointment?")).toBeInTheDocument();
+    // 5. Click the "Confirm" button on the confirmation.
+    fireEvent.click(queryByText(appointment, "Confirm"));
+    // 6. Check that the element with the text "Deleting" is displayed.
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+    // 7. Wait until the element with the "Add" button is displayed.
+    await waitForElement(() => getByAltText(appointment, "Add"));
+    // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+    expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
+  });
+
+  it("shows the save error when failing to save an appointment", () => {
+    axios.put.mockRejectedValueOnce();
   });
 
   
