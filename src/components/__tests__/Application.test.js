fd881f2b1774ed9e525edf733e1e22cb8b57def8
Ha Pham <hapham@Has-MacBook-Pro.local>
2019-11-04 20:02:06 -0500

    skipped the tests that renders without crashing and changed the promise in application test to async await

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -3,16 +3,21 @@ import React from "react";
 import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
 
 import Application from "components/Application";
-//import axios from "axios";
+import { exportAllDeclaration } from "@babel/types";
 
 afterEach(cleanup);
 //We want to wait for the fake API request to complete before we confirm that the data has loaded
 //We can make our test asynchronous by returning a Promise.
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
+it("defaults to Monday and changes the schedule when a new day is selected", async () => {
   const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
-  });
+  // return waitForElement(() => getByText("Monday")).then(() => {
+  //   fireEvent.click(getByText("Tuesday"));
+  //   expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  // });
+
+  await waitForElement(() => getByText("Monday"));
+  fireEvent.click(getByText("Tuesday"));
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
+   
 });
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -40,11 +40,11 @@ export default function useApplicationData() {
     //goes into api endpoint to permanently add appointment
     return axios.put(`/api/appointments/${id}`, { interview }).then(() => {
       // dispatch({ type: SET_INTERVIEW, value: { appointments } })
-      axios
+      Promise
         .all([
-          axios.get(`http://localhost:8001/api/days`),
-          axios.get(`http://localhost:8001/api/appointments`),
-          axios.get(`http://localhost:8001/api/interviewers`)
+          axios.get(`/api/days`),
+          axios.get(`/api/appointments`),
+          axios.get(`/api/interviewers`)
         ])
         .then(all => {
           let days = all[0].data;
@@ -72,7 +72,7 @@ export default function useApplicationData() {
     return axios.delete(`/api/appointments/${id}`).then(() => {
       //setState({ ...state, appointments })
       // dispatch({ type: SET_INTERVIEW, value: {appointments} })
-      axios
+      Promise
         .all([
           axios.get(`/api/days`),
           axios.get(`/api/appointments`),
