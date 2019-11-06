eaaeba9fa4290df53b5b393c55fe1a9351256498
Ha Pham <hapham@Has-MacBook-Pro.local>
2019-11-06 15:51:25 -0500

    Completed cpyress test for booking and interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,37 +1,47 @@
-//We will POST to our /api/debug/reset route to reset the database before each test
 describe("Appointments", () => {
-  it("should visit root", () => {
-    cy.visit("/");
-  });
-  it("should book an interview", () => {
+  beforeEach(() => {//Group common behaviour together
+    //Changed the server state, the second time this test runs, it will fail, must rest db to start fresh
+    cy.request("GET", "/api/debug/reset")
     cy.visit("/")
-    /*
-    Visits the root of our web server
-    Clicks on the "Add" button in the second appointment
-    Enters their name
-    Chooses an interviewer
-    Clicks the save button
-    Sees the booked appointment
-     */
+      // We can use the built-in retry behaviour of the contains command to verify that the text "Monday" is in the DOM
+    cy.contains("Monday")
+  })
+
+  it("should book an interview", () => {
+    // Clicks on the "Add" button in the second appointment
+    cy.get("[alt=Add]")
+    .first().click()
+    //search for the input
+    cy.get("[data-testid=student-name-input]")
+    // Enters their name
+    .type('Lydia Miller-Jones')
+    // Chooses an interviewer, watch for brackets!
+    cy.get("[alt='Sylvia Palmer']").click()
+    // Clicks the save button
+    cy.contains("Save").click()
+    // Sees the booked appointment
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+
     
   });
-  it("should edit an interview", () => {
-    cy.visit("/")
-    /*
-    Visits the root of our web server
-    Clicks the edit button for the existing appointment
-    Changes the name and interviewer
-    Clicks the save button
-    Sees the edit to the appointment
-     */
-  });
-  it("should cancel an interview", () => {
-    cy.visit("/")
-    /*
-    Visits the root of our web server
-    Clicks the delete button for the existing appointment
-    Clicks the confirm button
-    Sees that the appointment slot is empty
-     */
-  });
+  // it("should edit an interview", () => {
+  //   cy.visit("/")
+    
+  //   // Visits the root of our web server
+  //   // Clicks the edit button for the existing appointment
+  //   // Changes the name and interviewer
+  //   // Clicks the save button
+  //   // Sees the edit to the appointment
+     
+  // });
+  // it("should cancel an interview", () => {
+  //   cy.visit("/")
+    
+  //   // Visits the root of our web server
+  //   // Clicks the delete button for the existing appointment
+  //   // Clicks the confirm button
+  //   // Sees that the appointment slot is empty
+     
+  // });
 });
--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -5,7 +5,7 @@ describe("Navigation", () => {
   //find text that contains Tuesday and click on it
   it("should navigate to Tuesday", () => {
     cy.visit("/")
-    .contains("[data-testid=day]", "Tuesday")
+    cy.contains("[data-testid=day]", "Tuesday")
     .click()
     //has to find the selected item again because it disappears when clicked
     cy.contains("[data-testid=day]", "Tuesday")
