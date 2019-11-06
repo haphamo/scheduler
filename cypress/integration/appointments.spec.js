2a364c38019d9574f351dcddf39cc067b89a5ad0
Ha Pham <hapham@Has-MacBook-Pro.local>
2019-11-06 16:08:10 -0500

    completed should edit an interview test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -25,16 +25,20 @@ describe("Appointments", () => {
 
     
   });
-  // it("should edit an interview", () => {
-  //   cy.visit("/")
-    
-  //   // Visits the root of our web server
-  //   // Clicks the edit button for the existing appointment
-  //   // Changes the name and interviewer
-  //   // Clicks the save button
-  //   // Sees the edit to the appointment
-     
-  // });
+  it("should edit an interview", () => {
+    // Clicks the edit button for the existing appointment
+    cy.get("[alt=Edit]")
+    .first()
+    .click({ force: true });
+    // Changes the name and interviewer
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+    // Clicks the save button
+    cy.contains("Save").click()
+    // Sees the edit to the appointment
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
   // it("should cancel an interview", () => {
   //   cy.visit("/")
     
