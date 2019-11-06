8149073396e63030843d54aa2d43ed143fbef7b5
Ha Pham <hapham@Has-MacBook-Pro.local>
2019-11-06 14:04:43 -0500

    added second test in nav.spec.test

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,15 @@
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+  //find text that contains Tuesday and click on it
+  it("should navigate to Tuesday", () => {
+    cy.visit("/")
+    .contains("[data-testid=day]", "Tuesday")
+    .click()
+    //has to find the selected item again because it disappears when clicked
+    cy.contains("[data-testid=day]", "Tuesday")
+    .should("have.class", "day-list__item--selected")
+    
+  });
+});
