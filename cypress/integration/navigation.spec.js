4eb9fda8708b1cd16bcbb306646daa2f1a3cef1e
Ha Pham <hapham@Has-MacBook-Pro.local>
2019-11-06 14:59:26 -0500

    Setting up tests in nav.spec

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,3 +1,4 @@
+//We will POST to our /api/debug/reset route to reset the database before each test
 describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
@@ -11,5 +12,36 @@ describe("Navigation", () => {
     cy.contains("[data-testid=day]", "Tuesday")
     .should("have.class", "day-list__item--selected")
     
+  });
+  it("should book an interview", () => {
+    cy.visit("/")
+    /*
+    Visits the root of our web server
+    Clicks on the "Add" button in the second appointment
+    Enters their name
+    Chooses an interviewer
+    Clicks the save button
+    Sees the booked appointment
+     */
+    
+  });
+  it("should edit an interview", () => {
+    cy.visit("/")
+    /*
+    Visits the root of our web server
+    Clicks the edit button for the existing appointment
+    Changes the name and interviewer
+    Clicks the save button
+    Sees the edit to the appointment
+     */
+  });
+  it("should cancel an interview", () => {
+    cy.visit("/")
+    /*
+    Visits the root of our web server
+    Clicks the delete button for the existing appointment
+    Clicks the confirm button
+    Sees that the appointment slot is empty
+     */
   });
 });
